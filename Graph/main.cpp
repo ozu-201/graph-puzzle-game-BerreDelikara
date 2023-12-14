8da11c09b76ed4368b90614c5f9935e0e49cdb75
BerreDelikara <146889123+BerreDelikara@users.noreply.github.com>
2023-12-14 14:36:24 +0300

    Week 1

---
+++ Graph/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by bd034030 on 12/14/2023.
+//
+
+#include "main.h"
+#include "../LinkedList.h"
+#include "Graph.h"
+
+int main(){
+    LinkedList list3;
+    LinkedList list4;
+    LinkedList list5;
+list::Graph::readData("djjk",list3,list4 ,list5);
+
+
+
+
+
+}
---
+++ Node.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Olcay Taner YILDIZ on 6.03.2023.
+//
+
+#include "Node.h"
+
+Node::Node(int data) {
+    this->data = data;
+    this->next = nullptr;
+}
+Node::Node(string data2) {
+    this->data2 = data2;
+    this->next = nullptr;
+}
+
+
+void Node::setNext(Node *_next) {
+    this->next = _next;
+}
+
+Node *Node::getNext() {
+    return next;
+}
+
+int Node::getData() {
+    return data;
+}
+
+string Node::to_string() {
+    return ::to_string(data);
+}
